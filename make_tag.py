f1cd8e5ed5eef5017f9e8907c1f030039d6b1e71
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2021-10-29 01:41:11 +0900

    add make_tag.py

---
+++ make_tag.py
@@ -0,0 +1,21 @@
+# 목차 작성시 주의사항
+# 1. 띄어쓰기 하지 않기
+# 2. . 사용하지 않기
+
+# 사용방법
+# 1. 제목을 다 적는다. 제목 하나당 한 줄.
+# 2. 파이썬을 실행시킨다.
+# 3. 붙여넣는다.
+# 4. 다 붙여넣고 나서 엔터를 두 번 누른다.
+# 5. 생성된 목차를 원하는 곳에 붙여넣는다.
+
+words = list()
+
+while True:
+    word = input()
+    if word == '':
+        break
+    words.append(word)
+
+for word in words:
+    print('[%s](#%s)' % (word, word))
