dc26b2cb3bc689fab54ccf3ac162b4946ce40314
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2021-10-29 01:40:46 +0900

    modify files name

---
+++ codes/10-json.js
@@ -0,0 +1,54 @@
+// JSON
+// JavaScript Object Notation
+
+// 1. Object to JSON
+// stringify(obj)
+let json = JSON.stringify(true)
+console.log(json)
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+  name: 'tori',
+  color: 'white',
+  size: null,
+  birthDate: new Date(),
+  // symbol: Symbol("id"),
+  jump: () => {
+    console.log(`${this.name} can jump!`);
+  },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+// json = JSON.stringify(rabbit, ['name']);
+json = JSON.stringify(rabbit, ['name', 'color', 'size']);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'name' ? 'ellie' : value;
+  // return value;
+});
+console.log(json);
+
+// 2. JSON to Object
+// parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+
+// const obj = JSON.parse(json);
+const obj = JSON.parse(json, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  // return value;
+  return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump(); // 에러 발생
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
+// console.log(obj.birthDate);
